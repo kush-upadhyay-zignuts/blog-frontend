55b61104b409dd4499e6f8aa70ba78ac6ef35d4e
kush-upadhyay-zignuts <kushu@zignuts.com>
2025-04-11 17:15:59 +0530

    Update adminNew.jsx

--- src/adminNew.jsx
+++ src/adminNew.jsx
@@ -23,7 +23,7 @@ const AdminNew = () => {
   useEffect(() => {
       async function fetchCategory() {
         try {
-          const res = await axios.get(`/api/admin/blogs/new`, { withCredentials: true });
+          const res = await axios.get(`https://blog-backend-1-5vcb.onrender.com/api/admin/blogs/new`, { withCredentials: true });
          const data = res.data;       
           setCategory(res.data);
         } catch (err) {
@@ -51,7 +51,7 @@ const AdminNew = () => {
       data.append('description', formData.description);
       data.append('Image', formData.Image);
         console.log(data)
-      await axios.post('/api/admin/blogs/new', data, {
+      await axios.post('https://blog-backend-1-5vcb.onrender.com/api/admin/blogs/new', data, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
