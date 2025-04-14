eebbe6f9b56a99d13ac9624b8e4c59a788f26267
kush-upadhyay-zignuts <kushu@zignuts.com>
2025-04-14 11:13:09 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
     <div>
       <nav className="navbar navbar-expand-lg fixed-top nav-color ">
     <div className="container-fluid">
-      <a className="navbar-brand text-info" href="#">BlogVerse </a>
+      <a className="navbar-brand text-info" href="#">StoryHaven </a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
