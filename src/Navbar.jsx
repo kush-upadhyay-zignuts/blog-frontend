d7a8bf1976f9527578343a1d6ba328da8cfbb2a5
kush-upadhyay-zignuts <kushu@zignuts.com>
2025-04-18 15:38:24 +0530

    major changes

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
      const handleSignin = () => {
       navigate("/signin");
     };
-    handleSignup = () => {
+    const handleSignup = () => {
       navigate("/signup");
     };
 
