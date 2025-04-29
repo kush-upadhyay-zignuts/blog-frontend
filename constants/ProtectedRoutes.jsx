a8a0b6fa64a8b07932dcbff63b8f78d3ca6f3293
kush-upadhyay-zignuts <kushu@zignuts.com>
2025-04-29 12:51:54 +0530

    protected routes

--- constants/ProtectedRoutes.jsx
+++ constants/ProtectedRoutes.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 const ProtectedRoutes = () => {
   const token = localStorage.getItem("LoggedInUser");
   
-  return token ? <Outlet /> : <Navigate to="/signin" />;
+  return token ? <Outlet /> : <Navigate to="/signin" replace />;
 };
 
 export default ProtectedRoutes;
